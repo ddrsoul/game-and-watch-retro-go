a2a99e2f7feb46dd991a93acd14f3e648b82f37e
ddrsoul <107053842+ddrsoul@users.noreply.github.com>
2024-03-27 15:47:36 +0300

    some environment variables
    run with ". ./prepare_gnw.sh" to exectute in same bash as you are now

---
+++ prepare_gnw.sh
@@ -0,0 +1,7 @@
+export GCC_PATH=/bin
+export OPENOCD="/opt/openocd-git/bin/openocd"
+export GNW_TARGET=zelda
+export EXTFLASH_SIZE_MB=16
+export ENABLE_SCREENSHOT=0
+export SHARED_HIBERNATE_SAVESTATE=1
+export COVERFLOW=1
